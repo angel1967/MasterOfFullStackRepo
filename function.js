b8d19a9b6b2263fa603ab5827747875a05204f9a
angel1967 <castaned2001@hotmail.com>
2022-08-07 09:17:51 -0500

    Update function.js
    Creación de la rama Feature-1 y Añadir la Clase A con los atributos foo bar

--- function.js
+++ function.js
@@ -9,3 +9,18 @@ function myfunction()
 }  
 
 console.log("llegamos al punto 2 - Gracias")
+
+// Feature-1 Añadir la clase A con los atributos : foo bar
+class Person {
+    constructor(name) {
+      this.name = name;
+    }
+  }
+  
+  // crea un objeto
+  const person1 = new Person('Miguel');
+  const person2 = new Person('Castaneda');
+  console.log("Feature-1")
+  
+  console.log(person1.name); // Miguel
+  console.log(person2.name); // Castañeda
